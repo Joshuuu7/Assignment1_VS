ab2c4d6913d571372abe83132d95db5f92b1e809
JoshuaAaronFlores <JoshuaAaronFlores@users.noreply.github.com>
2019-01-28 13:00:10 -0600

    Option 1 partially correct.

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -8,33 +8,155 @@ using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
+    public enum ItemType
+    {
+        Helmet, Neck, Shoulders, Back, Chest,
+        Wrist, Gloves, Belt, Pants, Boots,
+        Ring, Trinket
+    };
+
+    public enum Race { Orc = 0, Troll = 1, Tauren = 2, Forsaken = 3 };
+
+    public class Player
+    {
+        readonly uint id;
+        readonly string name;
+        //readonly Racial race;
+        uint level;
+        uint guildID;
+        uint[] gear;
+        List<uint> inventory;
+        bool v1;
+        bool v2;
+    }
+
+    public class Item : IComparable
+    {
+        private readonly uint id;
+        private string name;
+        //private ItemType type;
+        private uint ilvl;
+        private uint primary;
+        private uint stamina;
+        private uint requirement;
+        private string flavor;
+
+        public Item ()
+        {
+
+            //string reader = "";
+            //using (StreamReader inFile = new StreamReader("players.txt"))
+            //{
+            //    while (!inFile.EndOfStream)
+            //    {
+            //        string playerName = "";
+            //        reader = inFile.ReadLine().ToString();
+            //        {
+            //            playerName = new string((from c in reader where char.IsLetter(c) select c).ToArray());
+            //        }
+            //    }
+            //}
+
+            name = "Newbie's Helmettttt";
+            ilvl = 0;
+            //type = 1;
+            primary = 10;
+            stamina = 10;
+            requirement = 1;
+            flavor = "Every adventure has humble beginnings!";
+        }
+
+        public Item( uint i, string nm, uint il, uint pr, uint st, uint rq, string flv )
+        {
+            id = i;
+            name = nm;
+            ilvl = il;
+            primary = pr;
+            stamina = st;
+            requirement = rq;
+            flavor = flv;
+        }
+
+        public uint ID
+        {
+            get { return id; }
+        }
+
+        //public ItemType Type
+        //{
+        //    get { return type; }
+        //    set { type = value; }
+        //}
+
+        public string Name // This is my public property
+        {
+            get { return name; } // The attribute
+            set { name = value; } // "value" is the default name to the assignment operand 
+        }
+
+        public uint ILVL
+        {
+            get { return ilvl; }
+            set { ilvl = value; }
+        }
+
+        public uint Primary
+        {
+            get { return primary; }
+            set { primary = value; }
+        }
+
+        public uint Stamina
+        {
+            get { return stamina; }
+            set { stamina = value; }
+        }
+
+        public uint Requirement
+        {
+            get { return requirement; }
+            set { requirement = value; }
+        }
+
+        public string Flavor
+        {
+            get { return flavor; }
+            set { flavor = value; }
+        }
+
+        public int CompareTo(object alpha)
+        {
+            Item itemObject = (Item)alpha;
+
+            int value = itemObject.Name.CompareTo(this.Name);
+
+            if (value == 1)
+                return 1;
+            else if (value == -1)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
     public class Program
     {
-        public enum Race { Orc, Troll, Tauren, Forsaken };
-
-        public enum ItemType
-        {
-            Helmet, Neck, Shoulders, Back, Chest,
-            Wrist, Gloves, Belt, Pants, Boots,
-            Ring, Trinket
-        };
-
         private static uint MAX_ILLVL = 360;
         private static uint MAX_PRIMARY = 200;
         private static uint MAX_STAMINA = 275;
         private static uint MAX_LEVEL = 60;
         private static uint GEAR_SLOTS = 14;
         private static uint MAX_INVENTORY_SIZE = 20;
-        private static string [] GUILDS = { "Knights of Cydonia", "Death and Taxes", "I Just Crit My Pants", "What Have We Here", "Big Dumb Guild", "Honestly", "Sacred Heart" };
 
         public static void Main(string[] args)
         {
             string welcome = "Welcome to the World of ConflictCraft: Testing Environment";
             string selectedOption = "";
+
             System.Console.WriteLine();
             System.Console.WriteLine(welcome +  "! \n");
              
-            while (selectedOption != "10")
+            while ( selectedOption != "10" || selectedOption != "q" || selectedOption != "Q" || selectedOption != "quit" || selectedOption != "Quit" || selectedOption != "exit" || selectedOption != "Exit" )
             {
                 PrintMainMenu();
 
@@ -48,6 +170,7 @@ namespace Assignment1
                         Console.Clear();
                         PrintAllPlayers();
                         PrintMainMenu();
+                        Console.ReadKey();
                     }
                     else if (slacker.CompareTo("2") == 0)
                     {
@@ -55,10 +178,12 @@ namespace Assignment1
                         Console.Clear();
                         PrintAllGuilds();
                         PrintMainMenu();
+                        Console.ReadKey();
                     }
                     else
                     {
                         outFile.WriteLine(slacker);
+                        Console.ReadKey();
                     }
                     Console.ReadKey();
                 }
@@ -89,14 +214,34 @@ namespace Assignment1
 
         public static void PrintAllPlayers()
         {
-            string [] nameArray = { "Dark Master", "Scobomb", "xXSephirothXx" };
-            string [] race = { "Forsaken", "Tauren", "Orc" };
-            int [] level = { 60, 60, 10 };
-            string[] guild = { "Big Dumb Guild", "Death and Taxes", "Death and Taxes" };
-            for ( int i = 0; i < nameArray.Length; )
+            //string [] race = { "Forsaken", "Tauren", "Orc" };
+            //int [] level = { 60, 60, 10 };
+            //string[] guild = { "Big Dumb Guild", "Death and Taxes", "Death and Taxes" };
+            //for ( int i = 0; i < nameArray.Length; )
+            //{
+            //    System.Console.WriteLine(String.Format(" Name: {0, -20} Race: {1, -10} Level: {2, -10} Guild: {3, -10} ", nameArray[i], race[i], level[i], guild[i]));
+            //    i++;
+            //}
+
+            string reader = "";
+            using (StreamReader inFile = new StreamReader("players.txt"))
             {
-                System.Console.WriteLine(String.Format(" Name: {0, -20} Race: {1, -10} Level: {2, -10} Guild: {3, -10} ", nameArray[i], race[i], level[i], guild[i]));
-                i++;
+                string playerName = "";
+                string raceType = "";
+                while (!inFile.EndOfStream)
+                {
+                    reader = inFile.ReadLine().ToString();
+                    {
+                        playerName = new string((from c in reader where char.IsLetter(c) select c).ToArray());
+    
+                        //string raceType = new string (reader.ToCharArray().Where(char.IsDigit));
+                        //raceType = new string(reader.ToCharArray().Where(char.IsDigit).ToArray());
+                        //int[] raceNumberCategory = Convert.ToInt32(raceType);
+                        //race = new Race((from c in reader where char.IsDigit(c) select c).ToArray()).ToString();
+
+                        System.Console.WriteLine(" Name: {0, -20} Race: {1, -10} Level: {2, -10} Guild: {3, -10}", playerName, /*((Race)raceNumberCategory).ToString()*/7, 3, 4);
+                    }
+                }
             }
             System.Console.WriteLine();
         }
@@ -112,9 +257,11 @@ namespace Assignment1
                 {
                     reader = inFile.ReadLine().ToString();
                     {
-                        // Code from https://stackoverflow.com/questions/7411438/remove-characters-from-c-sharp-string
+                        // Code reference: https://stackoverflow.com/questions/7411438/remove-characters-from-c-sharp-string
 
                         guild = new string((from c in reader where char.IsWhiteSpace(c) || char.IsLetter(c) select c ).ToArray());
+
+                        // Code reference: https://www.arclab.com/en/kb/csharp/replace-or-remove-char-in-string-by-index.html
                         char[] ch = guild.ToCharArray();
                         ch[0] = ' ';
                         guildWithoutSpaces = new string(ch);
